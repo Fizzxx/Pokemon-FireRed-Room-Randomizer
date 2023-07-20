6d4b15681265349a33edc820cfa5169dbe887231
Kameron Scott <kameron-scott@outlook.com>
2023-07-19 23:31:42 -0500

    Adds attributes to setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,8 @@ from setuptools import setup
 
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        name='pokeFR_room_rando',
+        version='0.1.2',
+        author='Kameron Scott, Nick Tumminaro'
+    )
